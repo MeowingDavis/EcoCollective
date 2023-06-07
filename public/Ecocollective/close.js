3ecf17367ad7d76111a3170b7f80b3bfbc226b8c
Davis Lang <s3775656@student.rmit.edu.au>
2023-06-07 16:08:33 +1000

    close and posts

--- public/Ecocollective/close.js
+++
@@ -1,6 +0,0 @@
-const closeBtn = document.querySelector(".close-btn");
-const parent = document.querySelector(".parent");
-
-closeBtn.addEventListener("click", function() {
-  parent.style.display = "none";
-});
